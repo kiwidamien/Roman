4619db43abfce546cbf86db20bc5590e9ecb9094
kiwidamien <djmartin@ucdavis.edu>
2019-01-01 20:25:03 -0800

    Added examples with reasonable docstrings

---
+++ roman/__init__.py
@@ -0,0 +1,5 @@
+from .roman import int_to_roman_string, roman_string_to_int
+from .temperature import convert, convert_all
+
+__version__ = '0.1.0'
+__author__ = 'Damien Martin'
--- roman/roman.py
+++ roman/roman.py
@@ -1,4 +1,6 @@
-ROMAN_SYMBOLS = [
+# We don't want these exposed to the end user,
+# so lead with an underscore instead
+_ROMAN_SYMBOLS = [
     ('M', 1000), ('CM', 900),
     ('D', 500), ('CD', 400),
     ('C', 100), ('XC', 90),
@@ -9,22 +11,45 @@ ROMAN_SYMBOLS = [
 ]
 
 def roman_string_to_int(numeral_string):
-    """
-    Converts a Roman numeral string to integer form
+    """Converts a numeral string representing a roman numeral to an integer.
+
+    Args:
+        numeral_string: represents a valid roman numeral. Must consist only of
+                        M, D, C, L, X, V, and I.
+
+    Returns:
+        An integer.
+
+    Examples:
+        >>> roman_string_to_int('V')
+        5
+        >>> roman_string_to_int('MMXIX')
+        2019
     """
     total = 0
-    for symbol, value in ROMAN_SYMBOLS:
+    for symbol, value in _ROMAN_SYMBOLS:
         while numeral_string.startswith(symbol):
             total += value
             numeral_string = numeral_string[len(symbol):]
     return total
 
 def int_to_roman_string(number):
-    """
-    Converts a positive integer into a Roman numeral
+    """Converts a positive integer into a Roman numeral.
+
+    Args:
+        number: a positive integer to be converted into a roman numeral
+
+    Returns:
+        The string representation of number.
+
+    Examples:
+        >>> int_to_roman_string(5)
+        'V'
+        >>> int_to_roman_string(2019)
+        'MMXIX'
     """
     result = ''
-    for symbol, value in ROMAN_SYMBOLS:
+    for symbol, value in _ROMAN_SYMBOLS:
         result += (number//value) * symbol
         number = number % value
     return result
